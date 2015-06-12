885fb04107f02efa9606f3911ba40c46bc49bc83
srmds <stevenramdas@gmail.com>
2015-06-12 18:14:55 +0200

    Initial Commit

---
+++ CoreDataCRUD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataCRUD
+//
+//  Created by c0d3r on 12/06/15.
+//  Copyright (c) 2015 io pandacode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
