c087196d62ce498ea27de369b475064e9b544e6a
srmds <stevenramdas@gmail.com>
2015-06-12 23:48:47 +0200

    add url attributes

--- CoreDataCRUD/EventManager.swift
+++ CoreDataCRUD/EventManager.swift
@@ -20,7 +20,7 @@ enum EventEntityAttributes : String {
     fb_url      = "fb_url",
     ticket_url = "ticket_url"
     
-    static let getAll = [eventId, title, date, venue, city, country, attendees]
+    static let getAll = [eventId, title, date, venue, city, country, attendees, fb_url,ticket_url]
 }
 
 /**
