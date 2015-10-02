ea670fe31aeb581b6380055bdbb993306a016a35
srmds <stevenramdas@gmail.com>
2015-10-03 01:49:01 +0200

    add attributes, update header doc

--- CoreDataCRUD/Event.swift
+++ CoreDataCRUD/Event.swift
@@ -7,10 +7,40 @@
 import Foundation
 import CoreData
 
+/**
+    Enum for Event Entity member fields
+*/
+enum EventAttributes : String {
+    case
+    eventId    = "eventId",
+    title      = "title",
+    date       = "date",
+    venue      = "venue",
+    city       = "city",
+    country    = "country",
+    attendees  = "attendees",
+    fb_url      = "fb_url",
+    ticket_url = "ticket_url"
+    
+    static let getAll = [
+        eventId,
+        title,
+        date,
+        venue,
+        city,
+        country,
+        attendees,
+        fb_url,
+        ticket_url
+    ]
+}
+
 @objc(Event)
 
+/**
+    The Core Data Model: Event
+*/
 class Event: NSManagedObject {
-    
     @NSManaged var attendees: AnyObject
     @NSManaged var city: String
     @NSManaged var country: String
@@ -20,5 +50,5 @@ class Event: NSManagedObject {
     @NSManaged var fb_url: AnyObject
     @NSManaged var ticket_url: AnyObject
     @NSManaged var title: String
-
 }
+
