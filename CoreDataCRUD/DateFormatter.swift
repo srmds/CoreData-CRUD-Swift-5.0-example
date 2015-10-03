2b0e280b13d2c30509118256839d4db61bc7a530
srmds <stevenramdas@gmail.com>
2015-10-04 01:24:55 +0200

    Add headerdocs

--- CoreDataCRUD/DateFormatter.swift
+++ CoreDataCRUD/DateFormatter.swift
@@ -8,8 +8,19 @@
 
 import Foundation
 
+/**
+    DateFormatter Utility class
+*/
 class DateFormatter {
     
+    /**
+        Get a NSDate formatted object from a given String.
+    
+        - Note: the dateFormat is an optional parameter, with default value: dd-MM-yyyy
+        - Parameter dateString: The Date String to parse.
+        - Parameter dateFormat: The output format of the NSDate object.
+        - Returns: NSDate formatted Date object
+    */
     class func getDateFromString(dateString:String, dateFormat:String = "dd-MM-yyyy") -> NSDate {
         let dateFormatter = NSDateFormatter()
         dateFormatter.dateFormat = "dd-MM-yyyy"
@@ -17,6 +28,14 @@ class DateFormatter {
         return dateFormatter.dateFromString(dateString)!
     }
     
+    /**
+        Get a formatted String from a given NSDate object
+        
+        - Note: the dateFormat is an optional parameter, with default value: dd-MM-yyyy
+        - Parameter date: The Date object to parse
+        - Parameter dateFormat: The output format of the output String
+        - Returns: String formatted output date String
+    */
     class func getStringFromDate(date:NSDate, dateFormat:String = "dd-MM-yyyy") -> String {
         let dateFormatter = NSDateFormatter()
         dateFormatter.dateFormat = dateFormat
