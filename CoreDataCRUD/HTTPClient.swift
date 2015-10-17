51dd7134d4ef16b650b375d9bb2cad7d0bab4fe8
Steven <steven@jongensvantechniek.nl>
2015-10-17 17:49:27 +0200

    remove unneeded counter and check, in query builder

--- CoreDataCRUD/HTTPClient.swift
+++ CoreDataCRUD/HTTPClient.swift
@@ -41,21 +41,15 @@ class HTTPClient {
     */
     func queryBuilder(params: Dictionary<String,AnyObject>) -> String {
         
-        var queryString:String = ""
-        var counter = 0
-        
+        var queryString:String = "?"
+
         for (key, value) in params {
-            if counter != 0 {
-                if params.count > 1{
-                    queryString.append("&")
-                }
-            } else {
-                queryString.append("?")
+            
+            if params.count > 1 {
+                queryString.append("&")
             }
             
-   
             queryString.append("\(key)=\(value)")
-            ++counter
         }
         
         return queryString
